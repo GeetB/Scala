65693895da96d35072826af5d2f69a8f6435981e
unknown <geet.bhalshankar@gmail.com>
2020-02-17 12:25:30 +0700

    Fiel operation continued

--- src/fileoperation/csvfileReaderMain.scala
+++ src/fileoperation/csvfileReaderMain.scala
@@ -1,12 +1,19 @@
 package fileoperation
 
 /*
-   A simple scala program which read data from given source file path and print one line at a time on the console
+   Author - Geet Bhalshankar
+   Description :
+   A simple scala program  to :
+   1. Read data from csv file including header
+   2. Read data from csv file excluding/dropping header
+   3. Read data from URL
  */
 object csvfileReaderMain extends App {
   plainRead("input_data/dummy_data.csv")
   println()
   plainReadWithoutHeader("input_data/dummy_data.csv")
+  println()
+  plainReadFromURL("https://en.wikipedia.org/wiki/Apache_Hadoop")
 
   // Reading a file containing header
   def plainRead(path:String) : Unit = {
@@ -16,15 +23,24 @@ object csvfileReaderMain extends App {
       println(line)
     }
   }
+
   //Reading a file and exclude header while reading it
   def plainReadWithoutHeader(path:String) : Unit = {
     println("Read a CSV file while excluding header .....")
     val fileReaderWithoutHeader = scala.io.Source.fromFile(path)
-    for (line <- fileReaderWithoutHeader.getLines.drop(1)){
+    for (line <- fileReaderWithoutHeader.getLines.drop(1)) {
       println(line)
     }
-
   }
+
+  //Reading data from particular URL
+  def plainReadFromURL(pathURL: String) : Unit = {
+      println("Reading from URL ....")
+      val abc = scala.io.Source.fromURL("https://en.wikipedia.org/wiki/Apache_Hadoop")
+      for (urlLine <- abc.getLines){
+        println(urlLine)
+      }
+    }
 
 }
 
