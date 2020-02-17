95755170aaebeb4d085838c173b8a845f0e54f7c
unknown <geet.bhalshankar@gmail.com>
2020-02-17 10:26:53 +0700

    File operation in Scala

--- src/fileoperation/csvfileReaderMain.scala
+++ src/fileoperation/csvfileReaderMain.scala
@@ -5,13 +5,27 @@ package fileoperation
  */
 object csvfileReaderMain extends App {
 
-  println("Reading CSV File ....")
+  plainRead("input_data/dummy_data.csv")
+  println()
+  plainReadWithoutHeader("input_data/dummy_data.csv")
 
-  val fileReader = scala.io.Source.fromFile("input_data/dummy_data.csv")
-
+  // Reading a file containing header
+def plainRead(path:String) : Unit = {
+  println("Simple Reading CSV File, One line at a time ....")
+  val fileReader = scala.io.Source.fromFile(path)
   for(line <- fileReader.getLines){
     println(line)
   }
-
+}
+ //Reading a file and exclude header while reading it
+  def plainReadWithoutHeader(path:String) : Unit = {
+    println("Read a CSV file while excluding header .....")
+    val fileReaderWithoutHeader = scala.io.Source.fromFile(path)
+    for (line <- fileReaderWithoutHeader.getLines.drop(1)){
+      println(line)
+    }
+  }
 
 }
+
+
