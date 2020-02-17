3a27f33b891ff8ed19ca5fc886ad0cb5861bfc1a
unknown <geet.bhalshankar@gmail.com>
2020-02-17 12:31:09 +0700

    collection update

---
+++ src/collectionoperation/collectionMain.scala
@@ -0,0 +1,9 @@
+package collectionoperation
+/*
+  Aautor : Geet Bhalshankar
+  Description :
+  This program is to work on different collection in Scala
+ */
+object collectionMain extends App {
+
+}
