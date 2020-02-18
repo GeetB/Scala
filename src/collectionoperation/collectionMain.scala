819b56741d313911826dc090159af7b86b3de417
unknown <geet.bhalshankar@gmail.com>
2020-02-18 11:00:25 +0700

    Collection - List

--- src/collectionoperation/collectionMain.scala
+++ src/collectionoperation/collectionMain.scala
@@ -1,9 +1,30 @@
 package collectionoperation
 /*
-  Aautor : Geet Bhalshankar
-  Description :
-  This program is to work on different collection in Scala
+    Author : Geet Bhalshankar
+    Description :
+    This program is to work on different collection in Scala
+    1. List
+    2. Tuple
+    3. Map
  */
 object collectionMain extends App {
 
+  // Declaring List and assigning values to list
+  val collectionList = List("12","13","40","AAA","bbbb","A210","399")
+  //Iterating over the list using for loop
+  for ( element <- collectionList) {
+    println(element)
+  }
+  //Iterating over the list using foreach
+  collectionList.foreach(x => println(x))
+
+  collectionList.foreach { element =>
+    try {
+      element.toInt
+    } catch  {
+        case e: NumberFormatException => "Number Format Exception ....!"
+        case _ => "something else"
+      }
+    }
 }
+
